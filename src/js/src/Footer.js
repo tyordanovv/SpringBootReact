3439a914a75428ebc283982e0bc1cf5f82378462
Tihomir Yordanov <t.yordanovv@gmail.com>
2022-05-27 00:57:52 +0200

    footer component created

---
+++ src/js/src/Footer.js
@@ -0,0 +1,13 @@
+import { Button } from "antd";
+import React from "react";
+import Container from "./Container";
+
+const Footer = (props) => (
+  <div>
+    <Container>
+      <Button type="primary">Add new student</Button>
+    </Container>
+  </div>
+);
+
+export default Footer;
