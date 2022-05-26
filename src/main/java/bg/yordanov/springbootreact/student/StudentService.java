43ebd3e50f0b95455a29f7e20c33ddae5aafd7d6
Tihomir Yordanov <t.yordanovv@gmail.com>
2022-05-26 21:40:06 +0200

    adding controller

--- src/main/java/bg/yordanov/springbootreact/student/StudentService.java
+++ src/main/java/bg/yordanov/springbootreact/student/StudentService.java
@@ -1,4 +1,22 @@
 package bg.yordanov.springbootreact.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
 public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository){
+        this.studentRepository = studentRepository;
+    }
+
+    public List<Student> getAllStudents(){
+        return studentRepository.selectAllStudents();
+    }
 }
