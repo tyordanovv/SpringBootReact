875a5806db85d686a733881ca76c317b4c1463e9
Tihomir Yordanov <t.yordanovv@gmail.com>
2022-05-27 13:48:56 +0200

    adding footer with add student button

---
+++ src/js/src/Footer.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Avatar, Button } from "antd";
+import Container from "./Container";
+import "./Footer.css";
+
+const Footer = (props) => (
+  <div className="footer">
+    <Container>
+      {props.numberOfStudents ? (
+        <Avatar
+          style={{ backgroundColor: "#f56a00", marginRight: "5px" }}
+          size="large"
+        >
+          {props.numberOfStudents}
+        </Avatar>
+      ) : null}
+      <Button type="primary" onClick={() => props.handleAddStudentEvent()}>
+        Add new student
+      </Button>
+    </Container>
+  </div>
+);
+
+export default Footer;
